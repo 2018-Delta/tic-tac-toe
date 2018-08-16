d35978901569a88fe6eeb2ac43aecafaaaceb5a9
Alyssa Hope <alyssamhope@gmail.com>
2018-08-16 14:09:49 -0700

    adds square click event and parent handler

--- src/Board.js
+++ src/Board.js
@@ -4,20 +4,30 @@ import Square from './Square'
 import './Board.css'
 
 class Board extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			squaresArray: ["", "", "", "", "", "", "", "", ""],
+			clickEventsCount: 0
+		}
+	}
+
+	updateClicks = (event, id) => {
+		console.log(event);
+		console.log(id);
+		this.setState({clickEventsCount: this.state.clickEventsCount + 1})
+	}
+
 	render() {
+		let squares = this.state.squaresArray.map((square, index) => {
+			console.log(square, index);
+			return (
+				<Square updateClicks={this.updateClicks} />
+			)
+		})
 		return (
 			<main>
-				<Square />
-				<Square />
-				<Square />
-
-				<Square />
-				<Square />
-				<Square />
-
-				<Square />
-				<Square />
-				<Square />
+				{squares}
 			</main>
 		);
 	}
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			id: 4
+		}
+	}
+
+	handleClick = (event) => {
+		console.log("I was clicked!!");
+		this.props.updateClicks(event, this.state.id)
+	}
+
 	render() {
 		return (
-			<section>
+			<section onClick={this.props.updateClicks}>
 				I am a square!
 			</section>
 		);
